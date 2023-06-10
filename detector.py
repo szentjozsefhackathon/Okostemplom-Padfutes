621f5bcc73f1ae022984fd1fb949ac751cee122c
gmester7 <miszorigergo@gmail.com>
2023-06-10 16:18:05 +0200

    Detector

--- detector.py
+++ detector.py
@@ -8,7 +8,7 @@ index = 10
 # Load the camera
 #cap = cv2.VideoCapture(cam)
 
-def get_real_time_footage():
+def get_real_time_footage(cap):
     _, img = cap.read()
 
     if img is None:
@@ -43,7 +43,7 @@ def apply_mask(img, mask):
     masked_image = cv2.bitwise_and(img, img, mask=mask)
 
     # Show the image
-    show_picture(img)
+    #show_picture(masked_image)
 
     # Return the number of connected components
     return masked_image
@@ -78,11 +78,11 @@ def prepare_image(img):
     brightness = 80
     img = cv2.addWeighted(img, 1, img, 0, brightness)
 
-    contrast = 50
+    contrast = 100
     img = cv2.addWeighted(img, contrast, img, 0, int(round(255*(1-contrast)/2)))
 
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img = create_edge_image(img)
+    #img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #img = create_edge_image(img)
     #img = cv2.dilate(img, None, iterations=1)
     #img = cv2.erode(img, None, iterations=1)
     return img
@@ -92,23 +92,33 @@ def prepare_image_2(img):
     img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 
     return img
+
+def detect_person(img):
+    # Remove white colors from the image
+    img = cv2.inRange(img, (128, 0, 0), (255, 255, 255))
+
+    return img
+
+
 # Load an image
 #img = cv2.imread('images/ulo/l22u.jpg')
 img = cv2.imread('test-2.jpg')
 base = cv2.imread('images/edges-0.jpg')
 mask = cv2.imread('images/sector1_edge0.jpg')
 
+#img = get_real_time_footage(cap)
+
 # Prepare the image
-img = prepare_image_2(img)
-base = prepare_image(base)
+img = prepare_image(img)
 mask = prepare_mask(mask)
+
 # Apply the mask
-#img = apply_mask(img, mask)
+img = apply_mask(img, mask)
 
-#base = cv2.dilate(base, None, iterations=2)
-#img = cv2.subtract(img, base)
+#img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 
-img = remove_horizontal_lines(img)
+img = detect_person(img)
 
 # Show the image
 show_picture(img)
@@ -118,3 +128,4 @@ cv2.waitKey(0)
 
 # Close the window
 cv2.destroyAllWindows()
+#cap.release()
